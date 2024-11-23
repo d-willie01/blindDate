c456aee06e0fcc1564adcfef03ed82ceefbf6263
d-willie01 <dariusw0325@gmail.com0325>
2024-11-23 03:01:12 -0700

    designed onboard screens, not effective yet though

--- frontend/app/home/_layout.jsx
+++ frontend/app/home/_layout.jsx
@@ -8,7 +8,7 @@ export default function _layout() {
     }}>
         <Stack.Screen name='connect'/>
         <Stack.Screen name = "registration"/>
-        
+        <Stack.Screen name = "preferances"/>
     </Stack>
   )
 }
