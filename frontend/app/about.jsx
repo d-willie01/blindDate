23332793f4643c68b2c46dc0613ac2a16795093e
d-willie01 <dariusw0325@gmail.com0325>
2024-12-15 13:27:03 -0700

    issues with auto deletion of premium user object. updated logo to server hosted link

--- frontend/app/about.jsx
+++ frontend/app/about.jsx
@@ -11,18 +11,18 @@ export default function Page() {
         {/* Open Graph Meta Tags for Rich Previews */}
         <meta property="og:title" content="LiveLinkME" />
         <meta property="og:description" content="Chat and make real and lasting 1V1 connections!" />
-        <meta property="og:image" content="https://github.com/d-willie01/blindDate/blob/main/frontend/assets/images/flirting.png?raw=true" />
+        <meta property="og:image" content="https://livelinkme.com/assets/assets/images/logo.fa5ccdfd9dc10b147e403bded9b469c2.png" />
         <meta property="og:url" content="https://livelinkme.com" />
         <meta property="og:type" content="website" />
         
         {/* Twitter Card Meta Tags */}
         <meta name="twitter:title" content="LiveLinkME" />
         <meta name="twitter:description" content="Chat and make real and lasting 1V1 connections!" />
-        <meta name="twitter:image" content="https://github.com/d-willie01/blindDate/blob/main/frontend/assets/images/flirting.png?raw=true" />
+        <meta name="twitter:image" content="https://livelinkme.com/assets/assets/images/logo.fa5ccdfd9dc10b147e403bded9b469c2.png" />
         <meta name="twitter:card" content="summary_large_image" />
         
         {/* Favicon */}
-        <link rel="icon" href="https://raw.githubusercontent.com/d-willie01/blindDate/refs/heads/main/frontend/assets/images/logo.png" />
+        <link rel="icon" href="https://livelinkme.com/assets/assets/images/logo.fa5ccdfd9dc10b147e403bded9b469c2.png" />
       </Head>
       
       <Text>About LiveLinkME</Text>
