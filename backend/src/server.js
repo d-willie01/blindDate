2b386298401791d7ca45cc4c6a6208ea22a6139b
d-willie01 <dariusw0325@gmail.com>
2025-01-07 15:46:14 -0800

    fixed commented out code in server file

--- backend/src/server.js
+++ backend/src/server.js
@@ -60,11 +60,11 @@ app.use('/communications', communicationRouter);
 app.use('/analytics', analyticsRouter);
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => //console.log('MongoDB connected'))
+  .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 server.listen(PORT, () => {
-  //console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 
